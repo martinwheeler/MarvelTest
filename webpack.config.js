984eff3b5af3418b6363fd2cd390079d7a316d26
martinwheeler <martin@martinwheeler.com.au>
2018-07-19 08:23:44 +1000

    Update port to 80

--- webpack.config.js
+++ webpack.config.js
@@ -66,14 +66,14 @@ module.exports = {
 
   devServer: {
     proxy: { // proxy URLs to backend development server
-      '/api': 'http://localhost:9000'
+      '/api': 'http://localhost:80'
     },
     compress: true, // enable gzip compression
     historyApiFallback: true, // true for index.html upon 404, object for multiple paths
     hot: true, // hot module replacement. Depends on HotModuleReplacementPlugin
     https: false, // true for self-signed, object for cert authority
     noInfo: true, // only errors & warns on hot reload,
-    port: 9000,
+    port: 80,
     // ...
   },
 
